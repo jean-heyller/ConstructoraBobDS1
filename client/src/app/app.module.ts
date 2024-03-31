238dcf367248e792a662b2cd2dad18d8f7a2472c
JuanMiguel <juan.noriega@correounivalle.edu.co>
2024-03-31 15:58:57 -0500

    Se agrega el formulario para iniciar sesión

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/shared/form/form.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

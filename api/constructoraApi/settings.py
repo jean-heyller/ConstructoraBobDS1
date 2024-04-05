ba922ced8ed55fbbed4dc7ff001161b751eca8e8
blessed0314 <christian.daniel999@gmail.com>
2024-04-05 14:43:17 -0500

    Se configurta el gitignore para la carpeta api y se coloca variables de entorno

--- api/constructoraApi/settings.py
+++ api/constructoraApi/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 """
 Django settings for constructoraApi project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4aq3w0yj8ynwaswhpqwyjuw$)z)vz_drwhb3fez%2)nwytiyf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

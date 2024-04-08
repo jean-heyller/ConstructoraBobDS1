66e9844b3ab76557169a44fe5592daf55739ab29
blessed0314 <christian.daniel999@gmail.com>
2024-04-07 20:27:13 -0500

    Se fixea la función para validar recaptcha

--- api/usuario/serializers/recaptcha.py
+++ api/usuario/serializers/recaptcha.py
@@ -1,11 +1,13 @@
 import requests
+from decouple import config
 from rest_framework import serializers
 
 def validate_recaptcha(recaptcha_response):
     data = {
-        'secret': 'Tu clave privada de reCAPTCHA',
+        'secret': config('RECAPTCHA_PRIVATE_KEY'),
         'response': recaptcha_response
     }
+    print(data)
     r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
     result = r.json()
     if not result['success']:

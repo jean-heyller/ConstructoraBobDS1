75fcf495f6ff30415f221874f21335ae11fdb3d2
JuanMiguel <juan.noriega@correounivalle.edu.co>
2024-04-08 14:16:08 -0500

    Se agrega el componente que manejará la foto, integrado con cloudinary

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import {CloudinaryModule} from '@cloudinary/ng';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/shared/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/gerente/register/register.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
-
-import { FormsModule } from '@angular/forms';
 import { UsersComponent } from './components/gerente/users/users.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -30,6 +34,8 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    NgxDropzoneModule,
+    CloudinaryModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
